72f39bbd2748a6e26ae72ef989476d9fcee4ce9c
Anthony <anthony.kizer88@gmail.com>
2019-07-22 14:20:07 -0700

    finished functions

--- assignments/function-conversion.js
+++ assignments/function-conversion.js
@@ -5,26 +5,36 @@
 // };
 // myFunction();
 
+const myFunction = () => console.log("1.) Function was invoked!");
+myFunction();
+
 // let anotherFunction = function (param) {
 //   return param;
 // };
 // anotherFunction("Example");
+
+const anotherFunction = param => console.log(param);
+anotherFunction("2.) Example");
 
 // let add = function (param1, param2) {
 //   return param1 + param2;
 // };
 // add(1,2);
 
+const add = (param1, param2) => console.log("3.) ", param1 + param2);
+
+add(1, 2);
 // let subtract = function (param1, param2) {
 //   return param1 - param2;
 // };
 // subtract(1,2);
 
-
 // Stretch
 
-// exampleArray = [1,2,3,4];
+exampleArray = [1, 2, 3, 4];
 // const triple = exampleArray.map(function (num) {
 //   return num * 3;
 // });
-// console.log(triple);
+
+const triple = exampleArray.map(num => num * 3);
+console.log("4.) ", triple);
